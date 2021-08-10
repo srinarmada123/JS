606f8c52ee17e33287dec215c992b9cb7c7f5596
srinarmada123 <48949056+srinarmada123@users.noreply.github.com>
2021-08-10 16:57:57 +0530

    Add files via upload

---
+++ spread1.js
@@ -0,0 +1,6 @@
+	let arr=[1,2,3];
+			let arr2=[...arr];
+	document.write("The copied array is "+arr2);
+			let arr3=[...arr,45,67];
+	document.write("<br/>The copied array is "+arr3);	
+	document.write("<br/>Minimum value = "+Math.min(...arr3));
